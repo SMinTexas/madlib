ebc81cb6e4039c72142365e2917e68c3cef54d86
Steven Murray <tstevenm@gmail.com>
2019-08-29 14:58:43 -0500

    Initial Commit

---
+++ madlib.py
@@ -0,0 +1,9 @@
+#Prompt the user for the missing words to a Madlib sentence using the input function
+#Example: __(name)__'s favorite subject in school is __(subject)__.
+
+#My Madlib:  __(Title)__ is __(Name)__'s favorite movie of all time.
+
+name = input('What is your name? ')
+title = input('What is your favorite movie? ')
+
+print(f'{title} is {name}\'s favorite movie of all time.')
